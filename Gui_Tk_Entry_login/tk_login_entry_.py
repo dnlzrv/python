8149e5ea87156aff76392ec68cfd389888467e36
Di Az <diana.azov@outlook.com>
2023-03-02 11:43:24 +0100

    button['state']  disabled to normal

--- Gui_Tk_Entry_login/tk_login_entry_.py
+++ Gui_Tk_Entry_login/tk_login_entry_.py
@@ -2,6 +2,7 @@
 # 1) Label: oben, text='Login'
 # 2) Frame: mit Inhalt: Label + Entry für Benutzername + Passwort
 # 3) Button: text='Login'
+# 4) 2 Radiobuttons nebeneinander: "Remember me", "Don't remember me"
 
 import tkinter as tk 
 
@@ -9,7 +10,7 @@ class LoginApp(tk.Tk):
     def __init__(self):
         super(LoginApp, self).__init__()
         self.geometry('300x300')
-        self.title('Einloggen')
+        self.title('Einlogen')
         
         self.username = tk.StringVar()
         self.userpass = tk.StringVar()
@@ -21,22 +22,33 @@ class LoginApp(tk.Tk):
 
         self.entryUser = tk.Entry(self, textvariable= self.username).grid(row= 1, column= 2, columnspan=3, sticky='nesw')
         
+
         self.labelPass = tk.Label(self, text= 'Passwort').grid(row= 2, column= 1)
         self.userpass.trace("w", lambda name, index, mode, sv=self.userpass: self.onPasswordChanged(self.userpass))
         self.entryPass = tk.Entry(self, textvariable= self.userpass, show="*").grid(row= 2, column= 2, columnspan=3, sticky='nesw')
        
-        self.buttonSubmit =tk.Button(self, text= 'Submit', command = self.onButtonClick, state= tk.NORMAL).grid(row= 3, column= 2, columnspan=3)
+        self.buttonSubmit =tk.Button(self, text= 'Submit', command = self.onButtonClick, state= tk.DISABLED)
+        self.buttonSubmit.grid(row= 3, column= 2, columnspan=3)
 
     def onUserNameChanged(self, sv):
+        self.updateSubmitButton()
         print(self.username.get())
         return True
     
     def onPasswordChanged(self, sv):
+        self.updateSubmitButton()
         print(self.userpass.get())
         return True
 
     def onButtonClick(self):
         print(self.username.get(), ':', self.userpass.get())
+    
+    def updateSubmitButton(self):
+    
+        if self.isUsernameValid() and self.isPasswortValid():
+            self.buttonSubmit['state'] = tk.NORMAL
+        else:
+            self.buttonSubmit['state'] = tk.DISABLED
         
     def isUsernameValid(self):
         return len(self.username.get()) >= 3
